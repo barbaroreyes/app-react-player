76b1ffd88afe2f7a2186a0a76826e4e8cf61221e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-17 21:01:19 -0500

    added-react-player

--- new-app/src/components/VIdeo.js
+++ new-app/src/components/VIdeo.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player';
 
 const Video = () => {
     return (
-        <div>
-            Video 
+        <div className="video">
+             <ReactPlayer
+             className='player'
+             controls
+             url='https://www.twitch.tv/videos/1052860129'
+             width='680px' height="1000px''"
+             />
         </div>
     )
 }
